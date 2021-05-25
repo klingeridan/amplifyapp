974f95485c35014af774df919a6b5651655bfd6e
Idan Klinger <idan.k@fiverr.com>
2021-05-25 18:07:38 +0300

    add another comment

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ ReactDOM.render(
 );
 
 // SOME COMMENT
+// ANOTEHR COMMENT
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
